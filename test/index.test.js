4d5d765c4cf57f8bde5a42496c6f7629e0590cf8
David Luecke <daff@neyeon.com>
2018-01-31 15:00:45 -0800

    Refactor to export service classes directly and update Readme (#35)

--- test/index.test.js
+++ test/index.test.js
@@ -1,104 +1,35 @@
 const { expect } = require('chai');
 const stripe = require('../lib');
 
+const CLASSES = ['Account',
+  'Balance',
+  'BankAccount',
+  'Card',
+  'Charge',
+  'Coupon',
+  'Customer',
+  'Dispute',
+  'Event',
+  'Invoice',
+  'InvoiceItem',
+  'Order',
+  'Payout',
+  'Plan',
+  'Product',
+  'Recipient',
+  'Refund',
+  'Sku',
+  'Subscription',
+  'Token',
+  'Transaction',
+  'Transfer',
+  'TransferReversal'
+];
+
 describe('feathers-stripe', () => {
-  it.skip('is CommonJS compatible', () => {
-    expect(typeof require('../lib')).to.equal('object');
-  });
-
-  it('es2015 compatible', () => {
-    expect(typeof stripe).to.equal('object');
-  });
-
-  it('supports accounts', () => {
-    expect(typeof stripe.account).to.equal('function');
-  });
-
-  it('supports balances', () => {
-    expect(typeof stripe.balance).to.equal('function');
-  });
-
-  it('supports bankAccounts', () => {
-    expect(typeof stripe.bankAccount).to.equal('function');
-  });
-
-  it('supports cards', () => {
-    expect(typeof stripe.card).to.equal('function');
-  });
-
-  it('supports charges', () => {
-    expect(typeof stripe.charge).to.equal('function');
-  });
-
-  it('supports coupons', () => {
-    expect(typeof stripe.coupon).to.equal('function');
-  });
-
-  it('supports customers', () => {
-    expect(typeof stripe.customer).to.equal('function');
-  });
-
-  it('supports disputes', () => {
-    expect(typeof stripe.dispute).to.equal('function');
-  });
-
-  it('supports events', () => {
-    expect(typeof stripe.event).to.equal('function');
-  });
-
-  it('supports invoiceItems', () => {
-    expect(typeof stripe.invoiceItem).to.equal('function');
-  });
-
-  it('supports invoices', () => {
-    expect(typeof stripe.invoice).to.equal('function');
-  });
-
-  it('supports orders', () => {
-    expect(typeof stripe.order).to.equal('function');
-  });
-
-  it('supports payouts', () => {
-    expect(typeof stripe.payout).to.equal('function');
-  });
-
-  it('supports plans', () => {
-    expect(typeof stripe.plan).to.equal('function');
-  });
-
-  it('supports products', () => {
-    expect(typeof stripe.product).to.equal('function');
-  });
-
-  it('supports recipients', () => {
-    expect(typeof stripe.recipient).to.equal('function');
-  });
-
-  it('supports refunds', () => {
-    expect(typeof stripe.refund).to.equal('function');
-  });
-
-  it('supports skus', () => {
-    expect(typeof stripe.sku).to.equal('function');
-  });
-
-  it('supports subscriptions', () => {
-    expect(typeof stripe.subscription).to.equal('function');
-  });
-
-  it('supports tokens', () => {
-    expect(typeof stripe.token).to.equal('function');
-  });
-
-  it('supports transactions', () => {
-    expect(typeof stripe.transaction).to.equal('function');
-  });
-
-  it('supports transfers', () => {
-    expect(typeof stripe.transfer).to.equal('function');
-  });
-
-  it('supports transferReversals', () => {
-    expect(typeof stripe.transferReversal).to.equal('function');
+  it('exports expected classes', () => {
+    CLASSES.forEach(name => {
+      expect(typeof stripe[name]).to.equal('function');
+    });
   });
 });
