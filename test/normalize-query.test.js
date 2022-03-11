5a969e660d6a99c69a7cb1d66ac5646dddea325a
DaddyWarbucks <techtbeau@gmail.com>
2022-03-11 12:22:54 -0700

    Add normalizeParams function

--- test/normalize-query.test.js
+++ test/normalize-query.test.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 
-const normalize = require('../lib/normalize-query');
+const { normalizeQuery } = require('../lib/normalize-query');
 
 describe('normalizeQuery', () => {
   describe('when $limit is present', () => {
@@ -13,19 +13,19 @@ describe('normalizeQuery', () => {
     };
 
     it('replaces $limit with limit', () => {
-      const query = normalize(params);
+      const query = normalizeQuery(params);
       expect(query.$limit).to.equal(undefined);
       expect(query.limit).to.equal(5);
     });
 
     it('does not modify original params', () => {
       const paramsCopy = Object.assign({}, params);
-      normalize(params);
+      normalizeQuery(params);
       expect(params).to.deep.equal(paramsCopy);
     });
 
     it('returns original query object params', () => {
-      const query = normalize(params);
+      const query = normalizeQuery(params);
       expect(query.name).to.equal('bob');
     });
   });
@@ -38,7 +38,7 @@ describe('normalizeQuery', () => {
           name: 'bob'
         }
       };
-      const query = normalize(params);
+      const query = normalizeQuery(params);
       expect(query).to.deep.equal({ name: 'bob' });
     });
   });
